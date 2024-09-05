7e2964b5cf4044d27d6cf78245eb1d5eb9d59955
nogueira <marcosrafael.nm62@gmail.com>
2024-09-04 21:49:45 -0300

    feat: cadastro integrado com o backend

--- PROJETO INTERDISCIPLINAR/FRONT/connec-fono/src/pages/RegisterPage.jsx
+++ PROJETO INTERDISCIPLINAR/FRONT/connec-fono/src/pages/RegisterPage.jsx
@@ -4,12 +4,65 @@ import viteLogo from '/vite.svg'
 //import './App.css'
 import '../styles/registerPage.css'
 import {Link} from "react-router-dom";
+import axios, {AxiosError} from "axios";
 import LoginPage from "./LoginPage.jsx";
+
+
+const BASE_URL = "http://localhost:8080"
+axios.defaults.baseURL = BASE_URL
 
 function RegisterPage() {
     const [buttonPlaceHolder, setButtonPlaceholder] = useState('insira seu RA')
+    const [registerForm, setRegisterForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+        cpf: "",
+        ra: "",
+    });
+
+    const handleFormEdit = async (e) => {
+
+        const {name, value} = e.target;
+        setRegisterForm({
+            ...registerForm,
+            [name]: value,
+        })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(registerForm)
+
+        const response = await axios.post(`/patient`, {
+                name: registerForm.name,
+                email: registerForm.email,
+                password: registerForm.password,
+                cpf: registerForm.cpf,
+                ra: isStudent ? registerForm.ra : null,
+                type: isStudent ? "aluno" : "externo"
+            },
+            {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                }
+            }).then(() => {
+            console.log("[RESPONSE SUCCESS]: " + response.status + response.data)
+            window.alert("Usuário cadastrado com sucesso :" + response.data);
+        })
+            .catch((err) => {
+                if (err instanceof AxiosError) {
+                    console.log(err.response.data)
+                }
+
+            })
+
+
+    }
+
     const [isStudent, setIsStudent] = useState(true);
-    // pra trocar o placeholder de RA pra Email e vice versa
+// pra trocar o placeholder de RA pra Email e vice versa
 
     return (
         <div id="main-container">
@@ -45,15 +98,26 @@ function RegisterPage() {
                         </div>
 
                     </div>
-                    <div id="form-input-container">
-                        <input placeholder="insira seu email" id="login-input"/>
-                        <input placeholder="insira sua senha" id="login-input"/>
-                        <input placeholder="insira seu ra" id="login-input" className="toggle-button" style={{display : isStudent? 'flex':'none' }}/>
-                        <input placeholder="insira seu nome" id="login-input"/>
-                        <input placeholder="insira seu cpf" id="login-input"/>
-                        <button> cadastrar </button>
 
-                    </div>
+                    <form id="form-input-container" onSubmit={handleSubmit}>
+                        <input name="email" placeholder="insira seu email" id="login-input"
+                               defaultValue={registerForm.email}
+                               onChange={event => handleFormEdit(event)}/>
+                        <input name="password" placeholder="insira sua senha" id="login-input"
+                               defaultValue={registerForm.password}
+                               onChange={event => handleFormEdit(event)}
+                               type="password"/>
+                        <input name="ra" placeholder="insira seu ra" id="login-input" value={registerForm.ra}
+                               onChange={event => handleFormEdit(event)} className="toggle-button"
+                               style={{display: isStudent ? 'flex' : 'none'}}/>
+                        <input name="name" placeholder="insira seu nome" id="login-input" value={registerForm.name}
+                               onChange={event => handleFormEdit(event)}/>
+                        <input name="cpf" placeholder="insira seu cpf" id="login-input" value={registerForm.cpf}
+                               onChange={event => handleFormEdit(event)}/>
+                        <button type="submit"> cadastrar</button>
+                    </form>
+
+
                     <div id="form-footer-container">
                         Já possui uma conta?
                         <Link to="/">
